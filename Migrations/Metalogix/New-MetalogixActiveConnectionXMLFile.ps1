51dca2026727c5d086662113199ef42b68a890ce
joerodgers <28455042+joerodgers@users.noreply.github.com>
2021-08-23 15:53:38 -0400

    updates

---
+++ Migrations/Metalogix/New-MetalogixActiveConnectionXMLFile.ps1
@@ -0,0 +1,43 @@
+﻿$assemblyPath = "C:\Program Files (x86)\Metalogix\Content Matrix Consoles\SharePoint Edition\Metalogix.Core.dll"
+
+$tenantAdminSiteUrl = "https://contoso-admin.sharepoint.com"
+
+$migrationAccounts = @{
+    'john.doe@contoso.com' = 'pass@word1'
+    'jane.doe@contoso.com' = 'pass@word2'
+}
+
+Add-Type -Path $assemblyPath
+
+$assembly = [System.Reflection.Assembly]::LoadFile($assemblyPath)
+
+$lineFormat = '<Connection NodeType="Metalogix.SharePoint.SPTenant, Metalogix.SharePoint, Version=9.1.0.1, Culture=neutral, PublicKeyToken=3b240fac3e39fc03" SharePointVersion="" UnderlyingAdapterType="" ShowAllSites="True" AdapterType="CSOM" Url="{0}" UserName="{1}" SavePassword="True" Password="{2}" ReadOnly="False" AuthenticationType="Metalogix.SharePoint.Adapters.CSOM2013.Authentication.Office365StandADFSInitializer" IsOAuthAuthentication="False"><Proxy Url="" IsProxyImportedFromBrowser="True"/></Connection>'
+
+$xml = New-Object System.Text.StringBuilder
+
+$null = $xml.AppendLine("<ConnectionCollection>")
+
+foreach( $migrationAccount in $migrationAccounts.GetEnumerator() )
+{
+    $username = $migrationAccount.Key
+    $password = $migrationAccount.Value
+
+    $securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+
+    if( $assembly.GetName().Version -lt "9.2" )
+    {
+        $encryptedPassword = [Metalogix.Cryptography]::EncryptText( $securepassword, [Metalogix.Cryptography+ProtectionScope]::CurrentUser, $null )
+    }
+    elseif( $assembly.GetName().Version -ge "9.2" )
+    {
+        $cryptographyService = New-Object Metalogix.CryptographyService 
+        $encryptedPassword = $cryptographyService.EncryptText( $securepassword, [Metalogix.ProtectionScope]::CurrentUser, $null )
+    }
+
+    $null = $xml.AppendFormat( $lineFormat, $tenantAdminSiteUrl, $username, $encryptedPassword )
+    $null = $xml.AppendLine()
+}
+
+$null = $xml.AppendLine("</ConnectionCollection>")
+
+$xml.ToString() | Set-Content "ActiveConnections.xml"
---
+++ SharePointOnline/Misc/Get-OneDriveForBusinessSiteOwnerObjectId.ps1
@@ -0,0 +1,138 @@
+﻿#Requires -Module PnP.PowerShell
+
+function Get-OneDriveForBusinessSiteOwnerObjectId
+{
+    [CmdletBinding()]
+    param
+    (
+        # OneDrive Site Url
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Identity,
+
+        # AAD App Principal Client/Application Id
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ClientId,
+
+        # SharePoint Tenant Name (contoso)
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Tenant,
+
+        # AAD App Principal Client/Application certificate thumbprint
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Thumbprint
+    )
+    
+    begin 
+    {
+        $Tenant = $Tenant -replace ".onmicrosoft.com", ""
+
+        if( $PSBoundParameters.ContainsKey("Identity") )
+        {
+            $urls = @($Identity)
+        }
+        else 
+        {
+            Write-Verbose "$(Get-Date) - Querying tenant for all OD4B URLs"
+
+            $connection = Connect-PnPOnline -Url "https://$Tenant-admin.sharepoint.com" -ClientId $ClientId -Thumbprint $Thumbprint -Tenant "$Tenant.onmicrosoft.com" -ReturnConnection -Verbose:$false
+
+            $sites = Get-PnPTenantSite -IncludeOneDriveSites -Connection $connection | Where-Object -Property Template -match "SPSPERS" 
+        
+            Disconnect-PnPOnline -Connection $connection
+        }
+    }
+    process 
+    {
+        $counter = 1
+
+        foreach( $site in $sites )
+        {
+            Write-Verbose "$(Get-Date) - $counter/$($sites.Count) - Processing $($site.Url)"
+
+            if( $site.LockState -ne "Unlock" )
+            {
+                # can't pull the AadObjectId of the owner on a locked site
+                [PSCustomObject] @{
+                    SiteUrl            = $site.Url
+                    LockState          = $site.LockState 
+                    UserName           = $site.Owner
+                    SharePointObjectId = ""
+                    AzureAdObjectId    = ""
+                    ObjectIdMismatch   = ""
+                }
+
+                continue
+            }
+
+            try 
+            {
+                # connect to the OD4B site
+                $connection = Connect-PnPOnline -Url $site.Url -ClientId $ClientId -Thumbprint $Thumbprint -Tenant "$Tenant.onmicrosoft.com" -ReturnConnection -Verbose:$false
+
+                # get the owner and the AadObjectId value from SPO
+                $site = Get-PnPSite -Includes Owner, Owner.AadObjectId, LockState
+    
+                # remove the claims prefix from the login name
+                $ownerUserPrincipalName = $site.Owner.LoginName -replace "i\:0\#\.f\|membership\|", ""
+    
+                $azureAdObjectId  = ""
+                $objectIdMismatch = $false
+
+                if( -not [string]::IsNullOrWhiteSpace($ownerUserPrincipalName) -and $null -ne $site )
+                {
+                    try
+                    {
+                        # get the owner AadObjectId value from Azure AD
+                        $azureAdUser = Get-PnPAzureADUser -Identity $ownerUserPrincipalName -Connection $connection 
+
+                        $azureAdObjectId = $azureAdUser.Id
+                        
+                        $objectIdMismatch = $azureAdUser.Id -ne $site.Owner.AadObjectId.NameId
+                    }
+                    catch
+                    {
+                        $azureAdObjectId  = "User not found"
+                        $objectIdMismatch = "Unknown"
+                    }
+                }
+
+                # result
+                [PSCustomObject] @{
+                    SiteUrl            = $site.Url
+                    LockState          = $site.LockState 
+                    UserName           = $ownerUserPrincipalName
+                    SharePointObjectId = $site.Owner.AadObjectId.NameId
+                    AzureAdObjectId    = $azureAdObjectId
+                    ObjectIdMismatch   = $objectIdMismatch
+                }
+            }
+            catch
+            {
+                Write-Error "Error processing $($site.Url). Error: $_"
+            }
+            finally
+            {
+                if( $connection )
+                {
+                    Disconnect-PnPOnline -Connection $connection
+                }
+            }
+
+            $counter++
+        }
+    }
+    end
+    {
+        
+    }
+}
+
+# requries Azure AD App Principal Permissions
+# Application > SharePoint > Sites.FullControl.All
+# Application > Graph > User.Read.All
+
+Get-OneDriveForBusinessSiteOwnerObjectId -ClientId $env:O365_CLIENTID -Tenant $env:O365_TENANT -Thumbprint $env:O365_THUMBPRINT | Export-Csv -Path "OneDriveForBusinessPUIDs.csv" -NoTypeInformation
